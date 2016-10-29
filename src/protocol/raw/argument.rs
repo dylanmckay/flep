771d6a350814af927dffbccccb0ac1c26ef47874
Dylan McKay <dylanmckay34@gmail.com>
2016-10-29 19:41:03 +1300

    Come up with a general command definition macro

---
+++ src/protocol/raw/argument.rs
@@ -0,0 +1,22 @@
+use std::io::prelude::*;
+use std::io;
+
+/// An argument to a command.
+pub trait Argument : Sized
+{
+    fn read(read: &mut BufRead) -> Result<Self, io::Error>;
+    fn write(&self, write: &mut Write) -> Result<(), io::Error>;
+}
+
+impl Argument for String
+{
+    fn read(read: &mut BufRead) -> Result<Self, io::Error> {
+        unimplemented!();
+    }
+
+    fn write(&self, write: &mut Write) -> Result<(), io::Error> {
+        unimplemented!();
+        Ok(())
+    }
+}
+
