ebdd459e1f92bea5d7b7390240a6ef1d6e8ca461
Dylan McKay <dylanmckay34@gmail.com>
2016-11-02 01:48:54 +1300

    Move server::Client::tick into a separate file

--- src/server/client/mod.rs
+++ src/server/client/mod.rs
@@ -2,7 +2,6 @@ use {Connection, DataTransfer, DataTransferMode, Error, Io};
 use server::{Session, session};
 use {server, protocol, connection};
 
-use std::net::ToSocketAddrs;
 use std::io::prelude::*;
 use std::io;
 use std;
@@ -11,6 +10,7 @@ use mio;
 use uuid::Uuid;
 
 mod handle_command;
+mod tick;
 
 /// An FTP client from the point-of-view of the FTP server.
 pub struct Client
@@ -168,64 +168,7 @@ impl Client
 
     pub fn tick(&mut self,
                 io: &mut Io) -> Result<(), Error> {
-        match self.session {
-            Session::Ready(ref mut session) => {
-                let active_transfer = std::mem::replace(&mut session.active_transfer, None);
-
-                if let Some(active_transfer) = active_transfer {
-                    let dtp = std::mem::replace(&mut self.connection.dtp, DataTransfer::None);
-
-                    self.connection.dtp = match dtp {
-                        DataTransfer::None => {
-                            assert_eq!(session.data_transfer_mode, DataTransferMode::Active);
-
-                            let addr = ("127.0.0.1", session.port.unwrap()).to_socket_addrs()?.next().unwrap();
-                            let stream = mio::tcp::TcpStream::connect(&addr)?;
-
-                            let token = io.allocate_token();
-                            io.poll.register(&stream, token,
-                                             mio::Ready::readable() | mio::Ready::hup() |
-                                             mio::Ready::writable(),
-                                             mio::PollOpt::edge())?;
-
-                            println!("Establishing a DTP connection for ACTIVE mode");
-
-                            // We aren't ready to send data just yet.
-                            session.active_transfer = Some(active_transfer);
-
-                            DataTransfer::Connecting {
-                                stream: stream,
-                                token: token,
-                            }
-                        },
-                        DataTransfer::Connected { mut stream, .. } => {
-                            println!("sent file");
-
-                            self.connection.send_command(&protocol::command::TYPE {
-                                file_type: active_transfer.file_type,
-                            })?;
-
-                            stream.write(&active_transfer.data)?;
-                            stream.flush()?;
-                            drop(stream);
-
-                            std::thread::sleep(std::time::Duration::from_millis(800));
-                            self.connection.send_reply(protocol::Reply::new(226, "Transfer complete"))?;
-
-                            DataTransfer::None
-                        },
-                        state => {
-                            // We aren't ready to send data just yet.
-                            session.active_transfer = Some(active_transfer);
-                            state
-                        },
-                    };
-                }
-
-                Ok(())
-            },
-            _ => Ok(())
-        }
+        tick::tick(self, io)
     }
 
     fn initiate_transfer(&mut self, transfer: server::Transfer)
