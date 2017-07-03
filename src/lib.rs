7ae782205428e0c7871b762b2ce3a5ae6bd2cd5e
Dylan McKay <me@dylanmckay.io>
2017-07-03 17:45:27 +1200

    Document crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+//! FTP server library.
+
 pub extern crate flep_protocol as protocol;
 
 extern crate mio;
@@ -8,7 +10,6 @@ extern crate error_chain;
 pub use self::credentials::Credentials;
 pub use self::errors::*;
 
-// Reexports.
 pub use protocol::FileType;
 
 pub mod server;
